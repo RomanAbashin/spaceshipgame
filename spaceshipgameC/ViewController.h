c5cea7ea7bf61a6199c975af4f8d1eae3e50f299
RomanAbashin <Roman@RAMBA.local>
2015-10-10 23:05:30 +0200

    First code

--- spaceshipgameC/ViewController.h
+++ spaceshipgameC/ViewController.h
@@ -8,8 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    IBOutlet UIImageView *friendlyShip;
+    IBOutlet UIImageView *enemyShip;
+    IBOutlet UIImageView *motherShip;
+    IBOutlet UIImageView *missile;
+    
+    IBOutlet UILabel *livesLabel;
+    IBOutlet UILabel *scoreLabel;
+    
+    IBOutlet UIButton *startButton;
+    
+    IBOutlet UITouch *touch;
+    
+    NSString *livesString;
+    NSString *scoreString;
+    
+    NSTimer *enemyMovementTimer;
+    NSTimer *missileMovementTimer;
+    
+    
+}
 
+-(IBAction)startGame:(id)sender;
 
 @end
 
